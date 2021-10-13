14b561250941413ed4a304f29d3f33da248d4bc6
Joan Ciprià <joancipria@protonmail.com>
2021-10-13 13:24:19 +0200

    Implement condom instructions action

--- actions/actions.py
+++ actions/actions.py
@@ -40,16 +40,13 @@ class ActionDarBienvenida(Action):
         dispatcher.utter_button_message(
             "Puedo ayudarte con cualquier pregunta relacionada con el VIH. Escríbeme directamente cualquier duda que tengas, y si no se te ocurre qué preguntar, prueba a pulsar sobre alguno de los siguientes botones 👇", buttons)
 
-        # dispatcher.utter_message("¡Hola! Soy Juan, encantado de conocerte! \n Puedo ayudarte con cualquier pregunta relacionada con el VIH. \n Dime, ¿en qué puedo ayudarte?")
-
         return []
 
-
-class ActionSaludarUsuario(Action):
-    """Greets the user with/without privacy policy"""
+class ActionComoUsarPreservativo(Action):
+    """Informa al usuario sobre como usar un preservativo"""
 
     def name(self) -> Text:
-        return "action_saludar_usuario"
+        return "action_como_usar_preservativo"
 
     def run(
         self,
@@ -57,44 +54,20 @@ class ActionSaludarUsuario(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[EventType]:
-        intent = tracker.latest_message["intent"].get("name")
-        name_entity = next(tracker.get_latest_entity_values("PER"), None)
 
-        if intent == "saludar" or (intent == "introducir_datos" and name_entity):
-            if name_entity and name_entity.lower() != "":
-                dispatcher.utter_message(
-                    response="utter_saludar_con_nombre", nombre=name_entity)
-                return []
-            else:
-                dispatcher.utter_message(response="utter_saludar")
-                return []
+        # Get condom_type entity
+        condom_type = next(tracker.get_latest_entity_values("condom_type"), None)
+        print(condom_type)
+
+        # If condom_type is empty, show male condom
+        if condom_type is None:
+            dispatcher.utter_message(response="utter_instrucciones_preservativo_masculino")
+        
+        # If not, show requested condom
+        if condom_type == "female_condom":
+            dispatcher.utter_message(response="utter_instrucciones_preservativo_femenino")
+        
+        if condom_type == "male_condom":
+            dispatcher.utter_message(response="utter_instrucciones_preservativo_masculino")
+        
         return []
-
-
-# class ActionSessionStart(Action):
-#     def name(self) -> Text:
-#         return "action_session_start"
-
-#     @staticmethod
-#     def fetch_slots(tracker: Tracker) -> List[EventType]:
-#         """Collect slots that contain the user's name and phone number."""
-
-#         slots = []
-#         return slots
-
-#     async def run(
-#       self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
-#     ) -> List[Dict[Text, Any]]:
-
-#         # the session should begin with a `session_started` event
-#         events = [SessionStarted()]
-#         dispatcher.utter_message(template="utter_bienvenida")
-
-#         # any slots that should be carried over should come after the
-#         # `session_started` event
-#         events.extend(self.fetch_slots(tracker))
-
-#         # an `action_listen` should be added at the end as a user message follows
-#         events.append(ActionExecuted("action_listen"))
-
-#         return events
